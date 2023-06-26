feee422bc19870def357f706ffdf1fcdd8598a22
タヌキ <Tanuki1707@gmail.com>
2023-06-26 21:57:19 +0700

    260623

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const app = express();
 
-app.use(express.static(path.dirname('.')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (request, response) => {
-      response.sendFile(path.resolve(path.dirname('.') + '/HTML/Home.html'), 'utf8');
+      response.sendFile(path.join(__dirname, 'public/html/home.html'), 'utf8');
 })
 
 const PORT = process.env.PORT || 1707;
