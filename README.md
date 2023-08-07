0f25bc30a10b71328070b41d3235adefd0dbe174
タヌキ <Tanuki1707@gmail.com>
2023-08-07 22:22:23 +0700

    restart project

--- README.md
+++
@@ -1,3 +0,0 @@
-# Tanuki-Center
-Last-update: 2023/07/03
-This site contains details and gateways to all of my products.
--- src/home/WantedPoster.tsx
+++
@@ -1,9 +0,0 @@
-import './Home.css'
-
-function WantedPoster() {
-    return <img id="WantedPoster" src="/img/WantedPoster.png" />
-}
-
-export {
-    WantedPoster
-}
