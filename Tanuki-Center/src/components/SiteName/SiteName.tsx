925f4992fa8957821f78ec575f5b7095d9f6dc03
タヌキ <Tanuki1707@gmail.com>
2023-08-19 14:05:11 +0700

    add Site Name

---
+++ Tanuki-Center/src/components/SiteName/SiteName.tsx
@@ -0,0 +1,12 @@
+//Styles
+import './SiteName.scss'
+
+const SiteName = () => {
+    return (
+        <>
+            <h1 className="SiteName" >TANUKI CENTER</h1>
+        </>
+    )
+}
+
+export default SiteName;
