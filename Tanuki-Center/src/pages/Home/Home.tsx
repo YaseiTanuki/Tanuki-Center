52be627cab37862bad6092c4f85b8412699b49c9
タヌキ <Tanuki1707@gmail.com>
2023-08-20 10:15:35 +0700

    change site name to logo

--- Tanuki-Center/src/pages/Home/Home.tsx
+++ Tanuki-Center/src/pages/Home/Home.tsx
@@ -1,14 +1,15 @@
 //Components
-import SiteName from "../../components/SiteName/SiteName";
+import Logo from "../../components/Logo/Logo";
 
 //Styles
 import './Home.scss'
 
 const Home = () => {
+    const [BrightLogo, DarkLogo] = Logo();
     return (
         <>
             <div className="Home">
-                <SiteName/>
+                <span><img className="LobbyLogo" src={BrightLogo} alt="" /></span>
             </div>
         </>
     )
