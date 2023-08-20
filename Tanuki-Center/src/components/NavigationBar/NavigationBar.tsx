111e08bff975120657631b53ee99dd59f39f0f58
タヌキ <Tanuki1707@gmail.com>
2023-08-20 18:18:42 +0700

    Change navigation bar color when scrolling

--- Tanuki-Center/src/components/NavigationBar/NavigationBar.tsx
+++ Tanuki-Center/src/components/NavigationBar/NavigationBar.tsx
@@ -1,7 +1,7 @@
 /*BEGIN IMPORT*/
 //Package
 import {NavLink} from 'react-router-dom'
-
+import { useState, useEffect } from "react";
 //Components
 import Logo from "../Logo/Logo";
 
@@ -10,16 +10,33 @@ import './NavigationBar.scss'
 /*END IMPORT*/
 const NavigationBar = () => {
     const [BrightLogo, DarkLogo] = Logo();
+    const [CurrentLogo, setCurrentLogo] = useState(BrightLogo);
+    const [TextColor, setTextColor] = useState("black");
+
+    const ChangeColor = () => {
+        if(window.scrollY < 720){
+            setTextColor("black")
+            return setCurrentLogo(BrightLogo);
+        }
+        setTextColor("white")
+        setCurrentLogo(DarkLogo);
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', ChangeColor);
+    }
+    )
+
     return (
         <>
         <div className="NavigationBar">
-            <span><img className="NavLogo" src={BrightLogo} alt="" /></span>
+            <span><img className="NavLogo" src={CurrentLogo} alt="" /></span>
             <nav className="NavigationLink">
                 <ul>
-                    <li><NavLink to="/">Lobby</NavLink></li>
-                    <li><NavLink to="/library">Library</NavLink></li>
-                    <li><NavLink to="/about">About</NavLink></li>
-                    <li><NavLink to="/support">Support</NavLink></li>
+                    <li><NavLink className={TextColor} to="/">LOBBY</NavLink></li>
+                    <li><NavLink className={TextColor} to="/library">LIBRARY</NavLink></li>
+                    <li><NavLink className={TextColor} to="/about">ABOUT</NavLink></li>
+                    <li><NavLink className={TextColor} to="/support">SUPPORT</NavLink></li>
                 </ul>
             </nav>
         </div>
