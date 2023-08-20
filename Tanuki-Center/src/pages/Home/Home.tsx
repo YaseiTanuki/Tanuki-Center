54791ed899e63a28860d917cbc1078c6232b53b7
タヌキ <Tanuki1707@gmail.com>
2023-08-20 18:19:26 +0700

    change main logo and background color when scrolling

--- Tanuki-Center/src/pages/Home/Home.tsx
+++ Tanuki-Center/src/pages/Home/Home.tsx
@@ -1,15 +1,34 @@
 //Components
+import { useEffect, useState } from "react";
 import Logo from "../../components/Logo/Logo";
 
 //Styles
 import './Home.scss'
 
-const Home = () => {
+const Home = () => { 
     const [BrightLogo, DarkLogo] = Logo();
+    const [CurrentLogo, setCurrentLogo] = useState(BrightLogo);
+    const [BackgroundColor, setbackgroundColor] = useState("BrightBackground")
+
+
+    const ChangeColor = () => {
+        if(window.scrollY < 720){
+            setbackgroundColor("BrightBackground")
+            return setCurrentLogo(BrightLogo);
+        }
+        setbackgroundColor("DarkBackground")
+        setCurrentLogo(DarkLogo);
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', ChangeColor);
+    }
+    )
+
     return (
         <>
-            <div className="Home">
-                <span><img className="LobbyLogo" src={BrightLogo} alt="" /></span>
+            <div className={'Home' + ' ' + BackgroundColor}>
+                <span><img className="LobbyLogo" src={CurrentLogo} alt="" /></span>
             </div>
         </>
     )
