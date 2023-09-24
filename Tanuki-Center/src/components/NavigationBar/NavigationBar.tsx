3639a95c63d464c2fe666d94c9a0a7991b702207
タヌキ <Tanuki1707@gmail.com>
2023-09-24 23:55:40 +0700

    remove background change feature

--- Tanuki-Center/src/components/NavigationBar/NavigationBar.tsx
+++ Tanuki-Center/src/components/NavigationBar/NavigationBar.tsx
@@ -1,43 +1,27 @@
 /*BEGIN IMPORT*/
 //Package
 import {NavLink} from 'react-router-dom'
-import { useState, useEffect } from "react";
+
 //Components
 import Logo from "../Logo/Logo";
 
 //Styles
 import './NavigationBar.scss'
+
 /*END IMPORT*/
 const NavigationBar = () => {
     const [BrightLogo, DarkLogo] = Logo();
-    const [CurrentLogo, setCurrentLogo] = useState(BrightLogo);
-    const [TextColor, setTextColor] = useState("black");
-
-    const ChangeColor = () => {
-        if(window.scrollY < 720){
-            // setTextColor("black")
-            // return setCurrentLogo(BrightLogo);
-            return;
-        }
-        setTextColor("white")
-        setCurrentLogo(DarkLogo);
-    }
-
-    useEffect(() => {
-        window.addEventListener('scroll', ChangeColor);
-    }
-    )
 
     return (
         <>
         <div className="NavigationBar">
-            <span><img className="NavLogo" src={CurrentLogo} alt="" /></span>
+            <span><img className="NavLogo" src={BrightLogo} alt="" /></span>
             <nav className="NavigationLink">
                 <ul>
-                    <li><NavLink className={TextColor} to="/">LOBBY</NavLink></li>
-                    <li><NavLink className={TextColor} to="/library">LIBRARY</NavLink></li>
-                    <li><NavLink className={TextColor} to="/about">ABOUT</NavLink></li>
-                    <li><NavLink className={TextColor} to="/support">SUPPORT</NavLink></li>
+                    <li><NavLink to="/">LOBBY</NavLink></li>
+                    <li><NavLink to="/library">LIBRARY</NavLink></li>
+                    <li><NavLink to="/about">ABOUT</NavLink></li>
+                    <li><NavLink to="/support">SUPPORT</NavLink></li>
                 </ul>
             </nav>
         </div>
