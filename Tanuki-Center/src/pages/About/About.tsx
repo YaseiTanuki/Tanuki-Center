1a0988cfa3bdac26f1d01e2eb2a1dd8f357b4c73
タヌキ <Tanuki1707@gmail.com>
2023-09-24 23:56:37 +0700

    remove background change feature

--- Tanuki-Center/src/pages/About/About.tsx
+++ Tanuki-Center/src/pages/About/About.tsx
@@ -1,17 +1,12 @@
-import { useContext } from "react";
-import BackgroundContext from "../../hooks/contexts/BackgroundContext";
 import './About.scss'
 import '../Home/Home.scss'
 
 const About = () => {
-    const {BackgroundColor} = useContext(BackgroundContext);
 
     return (
-        <>
-        <div className={"About" + " " + (BackgroundColor ? 'BrightBackground' : 'DarkBackground')}>
+        <div className="About">
             <p>This is About</p>
         </div>
-        </>
     )
 }
 
