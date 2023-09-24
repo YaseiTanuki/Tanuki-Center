5dc474ef60fd7625a415bfa905346ba0e7800e80
タヌキ <Tanuki1707@gmail.com>
2023-09-24 23:57:14 +0700

    remove background change feature

--- Tanuki-Center/src/pages/Home/Home.tsx
+++ Tanuki-Center/src/pages/Home/Home.tsx
@@ -4,24 +4,17 @@ import { useEffect, useState, useContext } from "react";
 //Components
 import Logo from "../../components/Logo/Logo";
 
-//hooks
-import BackgroundContext from "../../hooks/contexts/BackgroundContext";
-
 //Styles
 import './Home.scss'
 
 const Home = () => { 
     const [BrightLogo, DarkLogo] = Logo();
     const [CurrentLogo, setCurrentLogo] = useState(BrightLogo);
-    const {BackgroundColor, setBackgroundColor} = useContext(BackgroundContext);
-
 
     const ChangeColor = () => {
-        if(window.scrollY < 720){
-            return;
-        }
-        setBackgroundColor('DarkBackground')
-        setCurrentLogo(DarkLogo);
+        if(window.scrollY > 720) {
+            setCurrentLogo(DarkLogo);
+    }
     }
 
     useEffect(() => {
@@ -31,9 +24,7 @@ const Home = () => {
 
     return (
         <div className="Home">
-            <div className={BackgroundColor}>
-                <span><img className="LobbyLogo" src={CurrentLogo} alt="" /></span>
-            </div>
+            <span><img className="LobbyLogo" src={CurrentLogo} alt="" /></span>
         </div>
     )
 }
--- Tanuki-Center/src/pages/Library/Library.tsx
+++ Tanuki-Center/src/pages/Library/Library.tsx
@@ -1,8 +1,10 @@
+import './Library.scss'
+
 const Library = () => {
     return (
-        <>
+        <div className="Library">
             <p>This is Library</p>
-        </>
+        </div>
     )
 }
 
