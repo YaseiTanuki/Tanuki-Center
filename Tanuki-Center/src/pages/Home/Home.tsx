93bdf7d766faa016dd32bd475ce5d4ea4c3afcf2
タヌキ <Tanuki1707@gmail.com>
2023-10-02 21:56:15 +0700

    remove logo in home page

--- Tanuki-Center/src/pages/Home/Home.tsx
+++ Tanuki-Center/src/pages/Home/Home.tsx
@@ -1,30 +1,11 @@
-//Package
-import { useEffect, useState } from "react";
-
-//Components
-import Logo from "../../components/Logo/Logo";
-
 //Styles
 import './Home.scss'
 
 const Home = () => { 
-    const [BrightLogo, DarkLogo] = Logo();
-    const [CurrentLogo, setCurrentLogo] = useState(BrightLogo);
-
-    const ChangeColor = () => {
-        if(window.scrollY > 720) {
-            setCurrentLogo(DarkLogo);
-    }
-    }
-
-    useEffect(() => {
-        window.addEventListener('scroll', ChangeColor);
-    }
-    )
 
     return (
         <div className="Home">
-            <span><img className="LobbyLogo" src={CurrentLogo} alt="" /></span>
+            <div>Home</div>
         </div>
     )
 }
